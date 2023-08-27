c87bd53525836e94495136106760448086189f8f
jgatto28 <98623447+jgatto28@users.noreply.github.com>
2023-08-27 11:35:00 +0100

    Add files via upload

---
+++ My_first_project/calculator.py
@@ -0,0 +1,19 @@
+print("Earned amount:")
+print("Bubblegum: $202")
+print("Toffee: $118")
+print("Ice cream: $2250")
+print("Milk chocolate: $1680")
+print("Doughnut: $1075")
+print("Pancake: $80")
+print()
+print("Income: $5405.0")
+print('Staff expenses:')
+staff_expenses = int(input())
+print(staff_expenses)
+print('Other expenses:')
+other_expenses = int(input())
+print(other_expenses)
+print("Net income: ", 5405 - staff_expenses - other_expenses, sep='$')
+
+
+
